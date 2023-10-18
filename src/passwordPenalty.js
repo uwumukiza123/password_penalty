a189f1bf2f2031f7e2ba808be96e26bc93d22f10
uwumukiza123 <uwumukizalaetitia@gmail.com>
2023-10-18 11:41:38 +0200

    modifying password penalty

--- src/passwordPenalty.js
+++ src/passwordPenalty.js
@@ -6,15 +6,15 @@
  * @returns {number}
  */
 export default function penaltyPoints(password = "") {
-  if (!password || password == null) return 0;
+  if (!password) return 0;
   if (typeof password !== "string") password = String(password);
 
   // the following code checks if the sequences has two or more consecutive characters
   const regex = /([a-zA-Z0-9])\1{1,}/g;
-  const match = password.match(regex);
+  const matches = password.match(regex);
 
-  const points = match?.reduce((acc, val) => {
-    return acc + val.length <= 2 ? 1 : 2;
+  const points = matches?.reduce((accumulator, match) => {
+    return accumulator + match.length <= 2 ? 1 : 2;
   }, 0);
 
   return points || 0;
